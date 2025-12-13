09b1504867ad6669ed9522b419b3988b87b59fc2
tahajaiti <taha.jaiti@gmail.com>
2025-12-13 20:35:49 +0100

    FEAT: add error response class for standard responses

---
+++ backend/src/main/java/com/kyojin/tasks/core/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.kyojin.tasks.core;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Data
+@Builder
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Map<String, String> validationErrors;
+}
