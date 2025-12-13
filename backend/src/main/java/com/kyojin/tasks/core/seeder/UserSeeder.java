1fecbdff3bf8223d44bacc89c9e8d0159bb06e1c
tahajaiti <taha.jaiti@gmail.com>
2025-12-13 20:38:25 +0100

    FEAT: add user seeder with default user seeding

---
+++ backend/src/main/java/com/kyojin/tasks/core/seeder/UserSeeder.java
@@ -0,0 +1,32 @@
+package com.kyojin.tasks.core.seeder;
+
+import com.kyojin.tasks.entity.User;
+import com.kyojin.tasks.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class UserSeeder implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+        if (userRepository.count() == 0) {
+            User user = new User();
+            user.setName("Taha Jaiti");
+            user.setEmail("taha@admin.com");
+            user.setPassword(passwordEncoder.encode("password123"));
+
+            userRepository.save(user);
+
+            log.info("User Seeder: Default user created successfully.");
+        }
+    }
+}
