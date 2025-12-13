dbed77b86b5d027c18d567d8710c6fcf4aa091b1
tahajaiti <taha.jaiti@gmail.com>
2025-12-13 16:49:53 +0100

    FEAT: add dev and test profiles to the app config, and ensure tests use test profile

--- backend/src/test/java/com/kyojin/tasks/TasksApplicationTests.java
+++ backend/src/test/java/com/kyojin/tasks/TasksApplicationTests.java
@@ -2,8 +2,10 @@ package com.kyojin.tasks;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TasksApplicationTests {
 
 	@Test
