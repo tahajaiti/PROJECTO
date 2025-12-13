962aa68786c55ac088453664b5dafd76c20b003c
tahajaiti <taha.jaiti@gmail.com>
2025-12-14 00:27:19 +0100

    FEAT: add the project service interface

---
+++ backend/src/main/java/com/kyojin/tasks/service/ProjectService.java
@@ -0,0 +1,21 @@
+package com.kyojin.tasks.service;
+
+import com.kyojin.tasks.dto.request.CreateProjectRequest;
+import com.kyojin.tasks.dto.request.ProjectFilterDTO;
+import com.kyojin.tasks.dto.request.UpdateProjectRequest;
+import com.kyojin.tasks.dto.response.ProjectDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ProjectService {
+
+    ProjectDTO create(CreateProjectRequest request, Long userId);
+
+    Page<ProjectDTO> getAll(ProjectFilterDTO filter, Pageable pageable, Long userId);
+
+    ProjectDTO getById(Long projectId, Long userId);
+
+    ProjectDTO update(Long projectId, UpdateProjectRequest request, Long userId);
+
+    void delete(Long projectId, Long userId);
+}
