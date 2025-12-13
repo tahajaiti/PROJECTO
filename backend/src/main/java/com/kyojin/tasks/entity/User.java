f4307dfccbce53b1ee1935b59f807aff65ba6f30
tahajaiti <taha.jaiti@gmail.com>
2025-12-14 00:23:25 +0100

    FEAT: add validation to user entity

--- backend/src/main/java/com/kyojin/tasks/entity/User.java
+++ backend/src/main/java/com/kyojin/tasks/entity/User.java
@@ -1,12 +1,17 @@
 package com.kyojin.tasks.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "_user")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,13 +19,23 @@ import lombok.experimental.SuperBuilder;
 @SuperBuilder
 public class User extends BaseEntity {
 
+    @NotBlank(message = "Name cannot be empty")
     @Column(nullable = false)
     private String name;
 
+    @NotBlank(message = "Email cannot be empty")
+    @Email(message = "Email must be valid")
     @Column(nullable = false, unique = true)
     private String email;
 
+    @NotBlank(message = "Password cannot be empty")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     @Column(nullable = false)
     private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Builder.Default
+    @ToString.Exclude
+    private List<Project> projects = new ArrayList<>();
 
 }
