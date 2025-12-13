c5a5882be0bc7497a9fa67c8f2cb204064b06994
tahajaiti <taha.jaiti@gmail.com>
2025-12-13 19:20:56 +0100

    FEAT: implement user details service with email loading

---
+++ backend/src/main/java/com/kyojin/tasks/security/AppUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.kyojin.tasks.security;
+
+import com.kyojin.tasks.entity.User;
+import com.kyojin.tasks.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
+public class AppUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .authorities(Collections.emptyList())
+                .build();
+    }
+
+}
