6472f989c990dc89150f615989f7c502d3b5d6a5
tahajaiti <taha.jaiti@gmail.com>
2025-12-14 15:49:17 +0100

    FIX: change bg color in app layout

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 const AppLayout = () => {
 
     return (
-        <div className="min-h-screen bg-gray-50">
+        <div className="min-h-screen bg-zinc-950">
             <Header />
 
             <main className="w-full h-full">
