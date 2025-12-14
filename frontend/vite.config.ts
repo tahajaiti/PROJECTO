3b1c5a3000bbe20a67d3b4e629be884b192c2a04
tahajaiti <taha.jaiti@gmail.com>
2025-12-14 15:46:32 +0100

    FEAT: added zustand react hook form, zod, and improve nginx conf

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 
 // https://vite.dev/config/
@@ -13,4 +14,10 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@types': path.resolve(__dirname, 'src/types'),
+    },
+  },
 })
