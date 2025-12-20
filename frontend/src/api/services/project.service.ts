785efb9cced24dbb5c13f9980d12f9b28a94e2b2
tahajaiti <taha.jaiti@gmail.com>
2025-12-20 19:30:31 +0100

    FEAT: create the project service with crud methods

---
+++ frontend/src/api/services/project.service.ts
@@ -0,0 +1,43 @@
+import { CreateProjectRequest, Page, Project, ProjectFilter, UpdateProjectRequest } from "../../types";
+import api from "../client";
+
+const getAll = async (filter?: ProjectFilter, page = 0, size = 10) => {
+  const params = new URLSearchParams();
+
+  params.append("page", String(page));
+  params.append("size", String(size));
+
+  if (filter?.query) {
+    params.append("query", filter.query);
+  }
+
+  const { data } = await api.get<Page<Project>>(`/v1/projects?${params}`);
+  return data;
+};
+
+const getById = async (id: number) => {
+  const { data } = await api.get<Project>(`/v1/projects/${id}`);
+  return data;
+};
+
+const create = async (request: CreateProjectRequest) => {
+  const { data } = await api.post<Project>("/v1/projects", request);
+  return data;
+};
+
+const update = async (id: number, request: UpdateProjectRequest) => {
+  const { data } = await api.put<Project>(`/v1/projects/${id}`, request);
+  return data;
+};
+
+const remove = async (id: number) => {
+  await api.delete(`/v1/projects/${id}`);
+};
+
+export const projectService = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove,
+}
