a92107db42cc4a01a16a88bee188c0f21ef5ec9f
tahajaiti <taha.jaiti@gmail.com>
2025-12-20 19:29:42 +0100

    FEAT: create a error handler componenet with internal navigation based on error codes

---
+++ frontend/src/components/core/GlobalErrorHandler.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { useQueryClient } from "@tanstack/react-query"
+
+interface ErrorType {
+    code: string
+    message: string
+}
+
+export const GlobalErrorHandler = () => {
+    const navigate = useNavigate()
+    const queryClient = useQueryClient()
+
+    useEffect(() => {
+        return queryClient.getQueryCache().subscribe((event) => {
+            const error = event?.query?.state?.error as ErrorType | undefined
+
+            if (!error) return
+
+            switch (error.code) {
+                case "UNAUTHORIZED":
+                    navigate("/login")
+                    break
+                case "FORBIDDEN":
+                    navigate("/")
+                    break
+                default:
+                    break
+            }
+        })
+    }, [navigate, queryClient])
+
+    return null
+}
