eb908540c049e2b9d048e4663f39a41e7388247e
tahajaiti <taha.jaiti@gmail.com>
2025-12-20 19:29:16 +0100

    FEAT: improve query client to not retry on unauthorized or 404 requests, add confirm toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,31 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { RouterProvider } from "react-router-dom"
 import { router } from "./router"
+import ConfirmToast from "./components/core/ConfirmToast"
+import { AxiosError } from "axios"
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: (failureCount, error) => {
+        if (error instanceof AxiosError) {
+          const status = error.response?.status
+          if ([401, 403, 404].includes(status ?? 0)) {
+            return false
+          }
+        }
+
+        return failureCount < 3
+      }
+    },
+  },
+})
 
 
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
+      <ConfirmToast />
       <RouterProvider router={router} />
     </QueryClientProvider>
   )
