0e13bb1fdbb56f26753e4e4d913571fa931f3772
tahajaiti <taha.jaiti@gmail.com>
2025-12-21 19:03:08 +0100

    FEAT: add Lombok constructor annotations to TaskDTO

--- backend/src/main/java/com/kyojin/tasks/dto/response/TaskDTO.java
+++ backend/src/main/java/com/kyojin/tasks/dto/response/TaskDTO.java
@@ -1,12 +1,16 @@
 package com.kyojin.tasks.dto.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class TaskDTO {
     private Long id;
     private String title;
