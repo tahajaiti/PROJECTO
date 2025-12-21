11a58a63cde40a020bc02fb24c30821f9454432c
tahajaiti <taha.jaiti@gmail.com>
2025-12-21 18:58:03 +0100

    FIX: remove unused variable

--- frontend/src/config/index.ts
+++ frontend/src/config/index.ts
@@ -2,8 +2,5 @@ export const config = {
   api: {
     url: import.meta.env.VITE_API_URL || '/api',
     timeout: Number(import.meta.env.VITE_API_TIMEOUT) || 10000,
-  },
-  auth: {
-    tokenKey: import.meta.env.VITE_TOKEN_KEY || "token",
-  },
+  }
 } as const;
